96d40f3879582de32b7301ef40203759cab94f1a
Igors Koptevs <igors.koptevs@gmail.com>
2022-05-15 13:52:04 +0300

    Uncommented user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
         $this->call([
             LiftSeeder::class,
             AdminSeeder::class,
