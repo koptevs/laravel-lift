384c2d9963a0e4bed028736b49f2c2bfb36df05a
Igors Koptevs <igors.koptevs@gmail.com>
2022-05-20 20:52:12 +0300

    Added versioning to mix - removed from app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}?v=<?=time()?>" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
