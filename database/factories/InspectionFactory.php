15b0c14b64ea147ae4ad9224b35d91082721be89
Igors Koptevs <igors.koptevs@gmail.com>
2022-05-25 17:15:59 +0300

    Corrected inspection seeder

--- database/factories/InspectionFactory.php
+++ database/factories/InspectionFactory.php
@@ -45,8 +45,8 @@ class InspectionFactory extends Factory
 4.1 Lifta kabīnes apdare ir nolietota.
 4.1 Lifta kabīnē ir nolietots vadības aparāts.
 '
-                ],
-                [
+                    ,
+
                     '1.4 Mašīntelpas durvīm nav brīdinājuma uzraksta "Lifta mašīntelpa - bīstami".
 3.1 Mašīntelpā ir aprīkojums, kas nav saistīts ar liftu.
 3.6 Eļļas noplūde no lifta mašīnas reduktora.
@@ -57,8 +57,8 @@ class InspectionFactory extends Factory
 5.1 Šahtas bedrē nav trepju.
 5.3 Palielināta lifta kabīnes brīvkustība vadotnēs (vādkurpju ieliktņi ir nolietoti).
 7.1 Visos stāvos sprauga starp šahtas durvīm un aiļu apmalēm ir lielāka par 10 mm.'
-                ],
-                [
+                    ,
+
                     '1.4 Mašīntelpas durvīm nav brīdinājuma uzraksta "Lifta mašīntelpa - bīstami".
 3.1 Mašīntelpā ir aprīkojums, kas nav saistīts ar liftu.
 3.1 Nevar atvērt mašīntelpu no iekšpuses bez atslēgas ja tā ir aizverta no ārpuses ar atslēgu.
@@ -67,8 +67,8 @@ class InspectionFactory extends Factory
 5.1 Šahtas bedrē nav trepju.
 5.3 Palielināta lifta kabīnes brīvkustība vadotnēs (vādkurpju ieliktņi ir nolietoti).
 7.1 Visos stāvos sprauga starp šahtas durvīm un aiļu apmalēm ir lielāka par 10 mm.'
-                ],
-                [
+                    ,
+
                     '
 3.1 Nevar atvērt mašīntelpu no iekšpuses bez atslēgas ja tā ir aizverta no ārpuses ar atslēgu.
 3.4 Lifta kabīnes vadības aparāts ir nolietots.
@@ -79,8 +79,8 @@ class InspectionFactory extends Factory
 5.1 Šahtas bedrē nav trepju.
 5.3 Palielināta lifta kabīnes brīvkustība vadotnēs (vādkurpju ieliktņi ir nolietoti).
 7.1 Visos stāvos sprauga starp šahtas durvīm un aiļu apmalēm ir lielāka par 10 mm.'
-                ],
-                [
+                    ,
+
                     '1.4 Mašīntelpas durvīm nav brīdinājuma uzraksta "Lifta mašīntelpa - bīstami".
 3.1 Mašīntelpā ir aprīkojums, kas nav saistīts ar liftu.
 3.1 Nevar atvērt mašīntelpu no iekšpuses bez atslēgas ja tā ir aizverta no ārpuses ar atslēgu.
