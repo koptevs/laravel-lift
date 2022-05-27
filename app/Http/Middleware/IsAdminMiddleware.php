cd96284d4d7dc5cf7d254bf51eb1d18454d1ca25
Igors Koptevs <igors.koptevs@gmail.com>
2022-05-27 16:53:07 +0300

    Added LU notes to check later

--- app/Http/Middleware/IsAdminMiddleware.php
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdminMiddleware
 {
@@ -19,6 +20,11 @@ class IsAdminMiddleware
         if (!auth()->user()->is_admin) {
             abort(403);
         }
+
+//        if (Auth::user() && Auth::user()->is_admin){ // row in database
+//            return $next($request);
+//        }
+//        return  redirect()->route('welcome');
 
         return $next($request);
     }
