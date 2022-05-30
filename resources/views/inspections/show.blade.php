d78be974f934d21011060313f888d55e42a4df07
Igors Koptevs <igors.koptevs@gmail.com>
2022-05-30 15:59:34 +0300

    Edited inspections.show

--- resources/views/inspections/show.blade.php
+++ resources/views/inspections/show.blade.php
@@ -9,36 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <a href="{{ route('inspections.index') }}">
-                        <h2 class="text-4xl ">
-                            Go to all inspections.
-                        </h2>
-                    </a>
-                    <br>
-                    <p>{{ $inspection->inspection_date }}</p>
-                    <p>{{ $inspection->inspection_protocol }}</p>
-                    <p>{{ $inspection->inspection_label }}</p>
-                    <p>{{ $inspection->inspection_result }}</p>
-                    <p>{{ $inspection->inspection_participant_1_profession }}</p>
-                    <p>{{ $inspection->inspection_participant_1_name }}</p>
-                    <p>{{ $inspection->inspection_participant_2_profession }}</p>
-                    <p>{{ $inspection->inspection_participant_2_name }}</p>
-                    <p>{{ $inspection->inspection_type }}</p>
-                    Lifta Reg. Nr.: <span class="text-lg font-bold">{{ $lift->reg_number }}</span><br>
-                    Adrese: <span class="text-lg font-bold">{{ $lift->country }}, {{ $lift->city }},
-                        {{ $lift->street }} iela {{ $lift->house }} / {{ $lift->entrance }}</span><br>
 
-                    Lift manager: <span class="text-lg font-bold">{{ $lift->liftManager->name }}, reg.nr. {{ $lift->liftManager->reg_number }}</span><br>
-                    Lift type.: <span class="text-lg font-bold">{{ $lift->lift_type }}</span><br>
-                    Manufactured: <span class="text-lg font-bold">{{ $lift->manufacture_year }}</span>
-                    <h3 class="text-lg font-bold">
-                        Pārbaudē konttatētas neatbilstības:
-                    </h3>
-                    <p>{{ $inspection->incpection_notes }}</p>
-                    <br><br>
+                    <h2 class="font-bold text-xl mb-2">Pārbaudes dati</h2>
+                    @php  $d = new DateTime($inspection->inspection_date); @endphp
+                    <p><span class="font-bold">Inspekcijas datums: </span>{{ $d->format('d.m.Y') }}</p>
+                    <p><span class="font-bold">Pārbaudes tips: </span>{{ $inspection->inspection_type }}</p>
+                    <p><span class="font-bold">Protokola numurs: </span>{{ $inspection->inspection_protocol }}</p>
+                    <p><span class="font-bold">Uzlīmes numurs: </span>{{ $inspection->inspection_label }}</p>
+                    <p><span class="font-bold">Pārbaudē piedalījas: </span>{{ $inspection->inspection_participant_1_profession }} {{ $inspection->inspection_participant_1_name }}, {{ $inspection->inspection_participant_2_profession }} {{ $inspection->inspection_participant_2_name }}</p>
+                    <p><span class="font-bold">Pārbaudes rezultāts: </span>{{ $inspection->inspection_result }}</p>
+
+                    <h2 class="font-bold text-xl mb-2 mt-4">Lifta dati</h2>
+
+                    <p><span class="font-bold">Lifta tips: </span>{{ $lift->lift_type }}</p>
+                    <p><span class="font-bold">Lifta Reg. Nr.: </span>{{ $lift->reg_number }}</p>
+                    <p><span class="font-bold">Lifta uzstādīšanas gads: </span>{{ $lift->manufacture_year }}</p>
+                    <p><span class="font-bold">Lifts uzstādīts: </span>{{ $lift->country }}, {{ $lift->city }},
+                        {{ $lift->street }} iela {{ $lift->house }} / {{ $lift->entrance }}</p>
+                    <p><span class="font-bold">Lifta pārvaldnieks: </span>{{ $lift->liftManager->name }}, reg.nr. {{ $lift->liftManager->reg_number }}</p>
+
+
+
+                    <h2 class="font-bold text-xl mb-2 mt-4">Pārbaudē konstatētas neatbilstības:</h2>
+                    <p class="mb-6">{{ $inspection->incpection_notes }}</p>
+
                     <a href="{{ route('lifts.edit', $lift) }}">
                         <x-button>
-                            Edit
+                            Rediģēt
                         </x-button>
                         <x-links.link-info href="{{ route('inspections.index') }}" target="_blank">
                             Visas pārbaudes
